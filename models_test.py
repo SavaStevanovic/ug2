ac6f9906ecfa0395f924c14b50c6f0e5f82b83a2
zivadinac <djape93@gmail.com>
2019-04-16 20:23:24 +0200

    Rename models to their real names (instead of enumerated).

--- models_test.py
+++ models_test.py
@@ -12,12 +12,12 @@ from keras.layers import Input
 # this could also be the output a different Keras model or layer
 input_tensor = Input(shape=(224, 224, 3))  # this assumes K.image_data_format() == 'channels_last'
 
-model01 = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model02 = Xception(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model03 = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model04 = VGG19(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model05 = ResNet50(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model09 = InceptionResNetV2(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model10 = MobileNet(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model12 = DenseNet201(input_tensor=input_tensor, weights='imagenet', include_top=True)
-model13 = NASNetLarge(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#InceptionV3 = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#Xception = Xception(input_tensor=input_tensor, weights='imagenet', include_top=True)
+VGG16 = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#VGG19 = VGG19(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#ResNet50 = ResNet50(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#InceptionResNetV2 = InceptionResNetV2(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#MobileNet = MobileNet(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#DenseNet201 = DenseNet201(input_tensor=input_tensor, weights='imagenet', include_top=True)
+#NASNetLarge = NASNetLarge(input_tensor=input_tensor, weights='imagenet', include_top=True)
